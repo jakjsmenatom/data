6e829dfac287a8ef4cb1196b26aa340d2b487ce8
Vlada Dusek <v.dusek96@gmail.com>
2022-03-25 18:42:35 +0100

    Update

--- data_mining/linear_regression.py
+++ data_mining/linear_regression.py
@@ -1,38 +1,70 @@
 #!.venv/bin/python
 
-import numpy as np
 from matplotlib import pyplot as plt
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error
 
-from utils import get_data
+from utils import Data, Sample, to_col_array  # noqa: WPS347
 
 NAME = __file__.split('/')[-1].split('.')[-2]
 PREDICTED_VALUES = 10
 
+
 if __name__ == '__main__':
-    years, values = get_data(filename='jjnt_wb_hdp_swe.csv')
+    data = Data()
+    data.read_csv(filename='jjnt_wb_hdp_swe.csv')
+    data.normalize()
 
     # Apply linear regression
-    reg = LinearRegression().fit(years, values)  # start regression (X and y are both column vectors)
+    reg = LinearRegression()
+    reg.fit(data.get_years_array(), data.get_values_array())
 
     # Get fitted line
     slope = reg.coef_[0][0]
     intercept = reg.intercept_[0]
-    fittedline = slope * years + intercept
+    fittedline = slope * data.get_years_array() + intercept
 
     # Get predictions
-    preds = {'year': [], 'value': []}
+    data_preds = Data()
     for year in range(61, 61 + PREDICTED_VALUES):
-        value = reg.predict(np.array([year])[::, None])[0][0]
-        preds['year'].append(year)
-        preds['value'].append(value)
+        data_preds.samples.append(Sample(
+            year=year,
+            value=reg.predict(to_col_array([year]))[0][0],
+        ))
+
+    # Get MSE
+    y_pred = []
+    for year in range(0, data.get_years_list()[-1] + 1):
+        value = reg.predict(to_col_array([year]))[0][0]
+        y_pred.append([year, value])
+    y_true = [[sample.year, sample.value] for sample in data.samples]
+    mse = mean_absolute_error(y_true, y_pred)
+    print('MSE = {}'.format(mse))
 
     # Get plot
     fig, ax = plt.subplots()
 
-    ax.scatter(x=list(years), y=list(values), color='blue', label='Known data')
-    ax.scatter(preds['year'], preds['value'], color='green', label='Predicted data')
-    ax.plot(years, fittedline, color='red', label='Reggression line', linewidth=2)
+    ax.scatter(
+        x=data.get_years_list(),
+        y=data.get_values_list(),
+        color='blue',
+        label='Known data',
+    )
+
+    ax.scatter(
+        x=data_preds.get_years_list(),
+        y=data_preds.get_values_list(),
+        color='green',
+        label='Predicted data',
+    )
+
+    ax.plot(
+        data.get_years_list(),
+        fittedline,
+        color='red',
+        label='Reggression line',
+        linewidth=2,
+    )
 
     ax.set_xlabel('Rok')
     ax.set_ylabel('Hodnota')
--- data_mining/utils.py
+++ data_mining/utils.py
@@ -1,25 +1,50 @@
+from typing import Iterable, NamedTuple
+
 import numpy as np
 import pandas as pd
 
 
-def get_data(filename: str) -> tuple[list, list]:
-    # Load the dataset to the Data Frame
-    df = pd.read_csv(filename)
+def to_col_array(data: Iterable) -> np.array:
+    """
+    From array([1, 2, 3]) to array([[1], [2], [3]])
+    """
+    a = np.array(data)
+    return a[::, None]
 
-    # Get first values
-    df_year_min = df['year'][0]
-    df_value_min = df['value'][0]
 
-    # Normalize
-    df['year'] -= df_year_min
-    df['value'] /= df_value_min
+class Sample(NamedTuple):
+    year: int
+    value: float
 
-    # Convert pandas Series to the numpy Array
-    array_years = np.array(list(df['year']))
-    array_values = np.array(list(df['value']))
 
-    # Convert row vector to column vector
-    array_years = array_years[::, None]
-    array_values = array_values[::, None]
+class Data:
 
-    return array_years, array_values
+    def __init__(self) -> None:
+        self.samples = []
+        self.year_first = None
+        self.value_first = None
+
+    def read_csv(self, filename: str) -> None:
+        df = pd.read_csv(filename)
+        self.samples = [Sample(row.year, row.value) for _, row in df.iterrows()]
+        self.year_first = self.samples[0].year
+        self.value_first = self.samples[0].value
+
+    def normalize(self) -> None:
+        self.samples = [
+            Sample(
+                year=sample.year - self.year_first, value=sample.value / self.value_first,
+            ) for sample in self.samples
+        ]
+
+    def get_years_list(self) -> list[int]:
+        return [sample.year for sample in self.samples]
+
+    def get_values_list(self) -> list[float]:
+        return [sample.value for sample in self.samples]
+
+    def get_years_array(self) -> np.array:
+        return to_col_array(self.get_years_list())
+
+    def get_values_array(self) -> np.array:
+        return to_col_array(self.get_values_list())
