bd137eec4303f6efcdd46f141987a00514f090c6
Vlada Dusek <v.dusek96@gmail.com>
2022-03-26 12:47:18 +0100

    Add polynomial reg

--- data_mining/polynomial_regression.py
+++ data_mining/polynomial_regression.py
@@ -2,6 +2,7 @@
 
 from matplotlib import pyplot as plt
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import PolynomialFeatures
 
@@ -9,15 +10,26 @@ from utils import Data, Sample, to_col_array  # noqa: WPS347
 
 NAME = __file__.split('/')[-1].split('.')[-2]
 PREDICTED_VALUES = 10
-DEGREE = 2
+MAX_DEGREE = 3
 
-if __name__ == '__main__':
+
+def execute(degree: int) -> None:
     data = Data()
     data.read_csv(filename='jjnt_wb_hdp_swe.csv')
     data.normalize()
 
-    reg = make_pipeline(PolynomialFeatures(DEGREE), LinearRegression())
+    reg = make_pipeline(PolynomialFeatures(degree), LinearRegression())
     reg.fit(data.get_years_array(), data.get_values_array())
+
+    # Get MSE
+    y_pred = []
+    for year in range(0, data.get_years_list()[-1] + 1):
+        value = reg.predict(to_col_array([year]))[0][0]
+        y_pred.append([year, value])
+    y_true = [[sample.year, sample.value] for sample in data.samples]
+    mse = mean_absolute_error(y_true, y_pred)
+    print('Degree = {}'.format(degree))
+    print('MSE = {}'.format(mse))
 
     # Get predictions
     data_preds = Data()
@@ -26,19 +38,6 @@ if __name__ == '__main__':
             year=year,
             value=reg.predict(to_col_array([year]))[0][0],
         ))
-
-    # Get predictions
-    # preds_years = np.linspace(61, 61 + PREDICTED_VALUES, 10).reshape(-1, 1)
-    # preds_values = reg.predict(data.get_years_array())
-
-    # print(preds_years)
-    # print(preds_values)
-
-    # preds = {'year': [], 'value': []}
-    # for year in range(61, 61 + PREDICTED_VALUES):
-    #     value = reg.predict(year)
-    #     preds['year'].append(year)
-    #     preds['value'].append(value)
 
     # Get plot
     fig, ax = plt.subplots()
@@ -52,16 +51,27 @@ if __name__ == '__main__':
 
     ax.scatter(
         x=data_preds.get_years_list(),
-        y=data_preds.get_years_list(),
+        y=data_preds.get_values_list(),
         color='green',
         label='Predicted data',
     )
 
-    # ax.plot(years, fittedline, color='red', label='Reggression line', linewidth=2)
+    ax.plot(
+        data.get_years_array(),
+        reg.predict(data.get_years_array()),
+        color='red',
+        label='Reggression curve (degree {})'.format(degree),
+        linewidth=2,
+    )
 
     ax.set_xlabel('Rok')
     ax.set_ylabel('Hodnota')
     ax.legend()
 
     fig.tight_layout()
-    plt.savefig('{}.pdf'.format(NAME))
+    plt.savefig('{}_{}.pdf'.format(NAME, degree))
+
+
+if __name__ == '__main__':
+    for degree in range(1, MAX_DEGREE + 1):
+        execute(degree)
